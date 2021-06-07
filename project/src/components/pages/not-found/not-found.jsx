978e1c4c7520e5ecbe271e53868f1de74a5136ca
Владимир <nekro199107@gmail.com>
2021-06-07 19:48:35 +0300

    Add main-page link to not-found page

--- project/src/components/pages/not-found/not-found.jsx
+++ project/src/components/pages/not-found/not-found.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function NotFound() {
   return (
     <div className="page-content">
       <h1>404 error. This page not found :(</h1>
+      <Link to="/">
+        <span>Back to main page</span>
+      </Link>
     </div>
   );
 }
