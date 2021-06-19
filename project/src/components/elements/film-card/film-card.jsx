f8a7a2a513e2712c9aec2bb1013116cbd15416ac
Владимир <nekro199107@gmail.com>
2021-06-19 18:46:34 +0300

    Player variant 2

--- project/src/components/elements/film-card/film-card.jsx
+++ project/src/components/elements/film-card/film-card.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link} from 'react-router-dom';
 import filmProp from '../../pages/film/film.prop';
 import VideoPlayer from '../../elements/video-player/video-player';
@@ -8,25 +8,25 @@ FilmCard.propTypes = {
 };
 
 export default function FilmCard({film}) {
-  // const [isActive, setActive] = useState(false);
+  const [isActive, setActive] = useState(false);
 
-  // function handleFilmCardEnter() {
-  //   setActive(true);
-  // }
+  function handleFilmCardEnter() {
+    setActive(true);
+  }
 
-  // function handleFilmCardLeave() {
-  //   setActive(false);
-  // }
+  function handleFilmCardLeave() {
+    setActive(false);
+  }
 
   return (
     <article
       className="small-film-card catalog__films-card"
-      // onMouseEnter={handleFilmCardEnter}
-      // onMouseLeave={handleFilmCardLeave}
+      onMouseEnter={handleFilmCardEnter}
+      onMouseLeave={handleFilmCardLeave}
     >
-      {/* <div className="small-film-card__image"> */}
-      <VideoPlayer film={film} />
-      {/* </div> */}
+      <div className="small-film-card__image">
+        <VideoPlayer film={film} isActive={isActive} />
+      </div>
 
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${film.id}`}>{film.name}</Link>
--- project/src/components/elements/video-player/video-player.jsx
+++ project/src/components/elements/video-player/video-player.jsx
@@ -1,46 +1,30 @@
-import React, {useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
+import PropTypes from 'prop-types';
 import filmProp from '../../pages/film/film.prop';
 
 const PLAYER_TIMEOUT = 1000;
 
 VideoPlayer.propTypes = {
   film: filmProp,
+  isActive: PropTypes.bool.isRequired,
 };
 
-export default function VideoPlayer({film}) {
+export default function VideoPlayer({film, isActive}) {
   const videoRef = useRef();
 
-  let isActive = false;
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      isActive && videoRef.current.play();
+    }, PLAYER_TIMEOUT);
 
-  function playVideo() {
-    isActive && videoRef.current.play();
-  }
-
-  function handleFilmCardEnter() {
-    isActive = true;
-    videoRef.current.src = film.previewVideoLink;
-    setTimeout(playVideo, PLAYER_TIMEOUT);
-    // videoRef.current.play();
-  }
-
-  function handleFilmCardLeave() {
-    isActive = false;
-    videoRef.current.src = null;
-  }
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [isActive]);
 
   return (
-    <div
-      className="small-film-card__image"
-      onMouseEnter={handleFilmCardEnter}
-      onMouseLeave={handleFilmCardLeave}
-    >
-      <video
-        ref={videoRef}
-        poster={film.previewImage}
-        width="280"
-        height="175"
-        muted
-      />
-    </div>
+    isActive
+      ? <video src={film.previewVideoLink} ref={videoRef} width={280} height={175} poster={film.previewImage} muted />
+      : <img src={film.previewImage} alt={film.name} width={280} height={175} />
   );
 }
