aba0943ec835dcc47e1d32d8a33508df86ab6f89
Владимир <nekro199107@gmail.com>
2021-06-20 18:15:36 +0300

    Small mocks for comments

---
+++ project/src/mocks/comments.js
@@ -0,0 +1,39 @@
+const comments = [{
+  id: 1,
+  user: {
+    id: 4,
+    name: 'Kate Muir',
+  },
+  rating: 8.9,
+  comment: 'Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director\'s funniest and most exquisitely designed movies in years.',
+  date: '2019-05-08T14:13:56.569Z',
+},{
+  id: 2,
+  user: {
+    id: 1,
+    name: 'Igor Grom',
+  },
+  rating: 10.0,
+  comment: 'Best film in my life!',
+  date: '2019-06-08T14:13:56.569Z',
+},{
+  id: 3,
+  user: {
+    id: 2,
+    name: 'Kate Fler',
+  },
+  rating: 5.5,
+  comment: 'Not my type :(.',
+  date: '2020-05-08T14:13:56.569Z',
+},{
+  id: 4,
+  user: {
+    id: 5,
+    name: 'Jora Chibisov',
+  },
+  rating: 1.0,
+  comment: 'Just a trash',
+  date: '2019-06-08T14:13:56.569Z',
+}];
+
+export default comments;
